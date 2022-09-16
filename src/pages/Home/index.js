ee2dc871a2153ce6c0257dfd54a719518274ebd5
roychong <roychongdev14@gmail.com>
2022-09-15 20:54:36 -0600

    update home page

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,5 +1,25 @@
 import React from "react";
+import Content from "../../components/Content";
 
 export default function Home() {
-  return <div className="text-white">Home</div>;
+  return (
+    <Content>
+      <div className="text-white text-center">
+        <h1 className="text-3xl mb-20">
+          Hello, This is{" "}
+          <strong className="text-orange-600">Roy Chong's</strong> product for
+          demo API
+        </h1>
+        <br></br>
+        <div className="flex flex-col gap-5">
+          <div className="text-2xl p-2 bg-orange-500">Create NFT Contract</div>
+          <div className="text-2xl p-2 bg-orange-500">Show NFT Contracts</div>
+          <div className="text-2xl p-2 bg-orange-500">Approve NFT Contract</div>
+          <div className="text-2xl p-2 bg-orange-500">Mint NFT Token</div>
+          <div className="text-2xl p-2 bg-orange-500">Show NFT Tokens</div>
+          <div className="text-2xl p-2 bg-orange-500">Approve NFT Token</div>
+        </div>
+      </div>
+    </Content>
+  );
 }
